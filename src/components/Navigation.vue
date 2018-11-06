54c4b570ce6f9a5bf21abfe62900c60d00f32b52
hansel88 <hpn_x@hotmail.com>
2018-11-06 22:04:29 +0100

    Added nav file..

---
+++ src/components/Navigation.vue
@@ -0,0 +1,17 @@
+<template>
+<nav>
+
+</nav>
+</template>
+<script>export default {
+  name: 'memo-nav',
+  data () {
+    return {
+      title: 'Welcome to Memo'
+    }
+  }
+}</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
